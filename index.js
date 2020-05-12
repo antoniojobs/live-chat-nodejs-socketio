9d6d0325e8e533584b96ee0af39eeac28ea623b4
José Antonio <antonio.jjobs@gmail.com>
2020-05-11 22:07:20 -0300

    implementacao do broadcast realizada

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http); //adiciona o socketio
 
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello world</h1>');
+// });
+
+//manipulador de rota apontando para index.html
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+// io.on('connection', (socket) => {
+//     console.log('um ususario se conectou');
+// });
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//     });
+// });
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
 });
 
 http.listen(3000, () => {
